e8bdefd1f31978f2f23cbed63381d6ed4620921a
Virtus-inc <aliondesh@gmail.com>
2021-06-04 01:04:52 +0300

    Update 1:04 04.06.21

---
+++ src/components/_infoBox.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function infoBox() {
+
+    return (
+        <div className="container_info">
+            <h1 className="title">Алексей Карачинский</h1>
+            <div className="avatar_and_text">
+                <img src="" alt="image"/>
+                <div className="text_info">
+                    <p>Длительность<br />консультации</p>
+                    <p>50 минут</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default infoBox;
