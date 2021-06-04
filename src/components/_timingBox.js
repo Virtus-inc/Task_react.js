6263f63f0687b647e4042d919f5a5804cc9d6453
Virtus-inc <aliondesh@gmail.com>
2021-06-04 18:25:41 +0300

    Update style 18:25 04.06.21

--- src/components/_timingBox.js
+++ src/components/_timingBox.js
@@ -21,9 +21,35 @@ function TimingBox() {
                 </div>
             </div>
             <div className="date_picker">
-                <div className="select_date">
-                    
+                <div className="select_date active_select_date">
+                    <p className="text_date active_text_date">Сегодня</p>
+                    <p className="number_date active_number_date">26</p>
                 </div>
+                <div className="select_date">
+                    <p className="text_date">Ср</p>
+                    <p className="number_date">27</p>
+                </div>
+                <div className="select_date">
+                    <p className="text_date">Чт</p>
+                    <p className="number_date">28</p>
+                </div>
+                <div className="select_date">
+                    <p className="text_date">Пт</p>
+                    <p className="number_date">29</p>
+                </div>
+                <div className="select_date">
+                    <p className="text_date">Сб</p>
+                    <p className="number_date">30</p>
+                </div>
+            </div>
+
+            <h2 className="title_time">Свободное время</h2>
+            <div className="time_picker">
+                <p className="off_time">18:00</p>
+                <p className="off_time active_time">18:30</p>
+                <p className="off_time">19:00</p>
+                <p className="off_time">19:30</p>
+                <p className="off_time">20:00</p>
             </div>
         </div>
     )
